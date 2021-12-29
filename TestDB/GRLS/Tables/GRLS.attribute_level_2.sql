5e66b0909957f97e67a0d9df91dc1ddb14807c50
Daivd Perkind <joeperkins@outlook.com>
2021-12-29 20:29:56 +0000

    First changes

--- TestDB/GRLS/Tables/GRLS.attribute_level_2.sql
+++ TestDB/GRLS/Tables/GRLS.attribute_level_2.sql
@@ -6,6 +6,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+/*
 IF OBJECT_ID('GRLS.attribute_level_2', 'U') IS NOT NULL
 BEGIN
 	DROP INDEX IF EXISTS U_IDX_l1_l2 ON Grls.attribute_level2
@@ -38,4 +39,37 @@ GO
 ALTER TABLE GRLS.attribute_level_2 
 	ADD CONSTRAINT FK_attr_level_2_attr_level_1 FOREIGN KEY (l1_id) REFERENCES GRLS.attribute_level_1(l1_id)
 		ON DELETE CASCADE;
+GO
+*/
+
+BEGIN TRY
+
+	BEGIN TRANSACTION
+
+	ALTER TABLE GRLS.attribute_level_2
+		ADD scheme_id	int		NULL
+
+	DROP INDEX U_IDX_l1_l2 ON GRLS.attribute_level_2
+
+	CREATE UNIQUE CLUSTERED INDEX U_IDX_schemeid_l1id_l2id ON GRLS.attribute_level_2 (scheme_id, l1_id, l2_id) ON [PRIMARY];
+
+	ALTER TABLE GRLS.attribute_level_2
+		ADD CONSTRAINT FK_attr_level_2_attr_scheme FOREIGN KEY (scheme_id) REFERENCES GRLS.attribute_scheme(scheme_id)
+			ON DELETE NO ACTION
+
+	COMMIT TRANSACTION
+
+END TRY
+BEGIN CATCH
+   SELECT  
+		ERROR_NUMBER() AS ErrorNumber , 
+        ERROR_SEVERITY() AS ErrorSeverity,  
+        ERROR_STATE() AS ErrorState,  
+        ERROR_PROCEDURE() AS ErrorProcedure,  
+        ERROR_LINE() AS ErrorLine,  
+        ERROR_MESSAGE() AS ErrorMessage
+
+	ROLLBACK
+END CATCH
+
 GO
