58fb1d2f9f1a5d936d3bb63f97a6b50f4d9a2057
Daivd Perkind <joeperkins@outlook.com>
2022-01-02 13:20:27 +0000

    Interim commit

--- TestDB/GRLS/Scripts/attr_list.sql
+++ TestDB/GRLS/Scripts/attr_list.sql
@@ -6,6 +6,6 @@ SELECT TOP (1000)
 	l1.abbrev ,
 	l2.*
 FROM
-	Grls.attribute_level_2 l2
-	INNER JOIN Grls.attribute_level_1 l1
+	GRLS.attribute_level_2 l2
+	INNER JOIN GRLS.attribute_level_1 l1
 	ON l2.l1_id = l1.l1_id
