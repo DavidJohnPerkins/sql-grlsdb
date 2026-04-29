c1dcb785bb23e93e8354c172515f1f1330c41e8b
DavidJohnPerkins <DavidJohnPerkins@users.noreply.github.com>
2026-04-29 21:30:05 +0100

    Script change

--- TestDB/GRLS/Scripts/add_movie_model_json.sql
+++ TestDB/GRLS/Scripts/add_movie_model_json.sql
@@ -3,14 +3,20 @@ GO
 
 DECLARE @json	COMMON.json = '
 	{
-		"sobriquet":	"NENSI_B",
+		"sobriquet":	"ALTEA",
 		"update_type":	"C",
 		"movie_titles": [
-			{ "movie_title": "whitney-medina.mp4"},
-			{ "movie_title": "foxy-di.mp4"},
-			{ "movie_title": "kate-keira-teal.mp4"}
-
+			{ "movie_title": "antea.mp4"}
 		]
 	}
 '
 EXEC GRLS.c_model_movie_json @json, 1, 1
+
+
+--select * from GRLS.movie where title like '%carr%' or title like '%ivy%'
+--select * from GRLS.model where sobriquet like 'TIF%'
+--exec GRLS.r_movie_list '{"model_id": -1, "minimum_rating": 1}'
+--delete from GRLS.movie_model where id=1076
+
+--select * from GRLS.model_name where model_name like '%cand%'
+--select * from GRLS.model where id=1072
